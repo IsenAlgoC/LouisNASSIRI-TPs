18693c80cd8935916c2dc394d6e313ea3e8c22c6
LouisISEN <71766646+LouisISEN@users.noreply.github.com>
2020-12-18 19:37:51 +0100

    Add files via upload

---
+++ TP5/Exercice1/tab.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//prototypes des fonctions utilisées dans l'exercice 1
+int initTab(int* tab, int size);
+
+int afficheTab(int* tab, int size, int nbElts);
+
+int unavingtTab(int* tab, int size, int nbElts);
+
+int* ajoutElementDansTableau(int* tab, int* size, int* nbElts, int element);
